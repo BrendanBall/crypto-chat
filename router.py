f81c4cbd1b3b32a2b6f46f66d90d261122326040
wraithy <wraith.andrew@gmail.com>
2015-03-11 09:41:36 +0200

    router now registers names and clients can talk to each other

--- router.py
+++ router.py
@@ -2,16 +2,15 @@ import socket, select
 
 sockets = [] 
 port = 8001
-
-names = {}
+clients = {}
 
 def router():
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	server_socket.bind(("", port))
-	server_socket.listen(10)
+	router_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	router_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	router_socket.bind(("", port))
+	router_socket.listen(10)
 	
-	sockets.append(server_socket)
+	sockets.append(router_socket)
 	print("Server started on port %s" % port)
 
 	while True:
@@ -19,48 +18,46 @@ def router():
 		ready_to_read,ready_to_write,in_error = select.select(sockets,[],[],0)
 
 		for sock in ready_to_read:
-			if sock == server_socket: 
-				new_sock, addr = server_socket.accept()
+			if sock == router_socket: 
+				new_sock, addr = router_socket.accept()
 				sockets.append(new_sock)
 				print("Client (%s, %s) connected" % addr)
 			else:
-				#try:
+				try:
 					# get messages from the client
 					data = sock.recv(4096).decode()
 					if data:
 						print(data)
 						# there is something in the socket
 						if data[:5] == "/name":
-							names[data[6:].strip()] = sock
-							print("%s is now online" % data[6:])
+							name = data[6:].strip()
+							clients[name] = sock
+							send("Router", sock, "You are now known as %s" % name)
+							print("%s is now online" % name)
 						else:
-							send(sock, data)  
+							sep = data.find(":")
+							send(get_name(sock), clients[data[:sep]], data[sep+1:].strip())
 					else:
-						# remove the socket that's broken    
+						# remove the socket that's broken
 						if sock in sockets:
 							sockets.remove(sock)
 
 						# at this stage, no data means probably the connection has been broken
-						#broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr) 
-				#except Exception(e):
-				#	print("an error occurred",e)
-				#	continue
+						#broadcast(router_socket, sock, "Client (%s, %s) is offline\n" % addr) 
+				except Exception as e:
+					print("an error occurred",e)
+					continue
 
-	server_socket.close()
+	router_socket.close()
 
-def send(sender, data):
-	# find sender name
-	sender_name = "OOPS"
-	for key, val in names.items():
-		if val == sender:
-			sender_name = key
-	sep = data.find(":")
-	receiver = data[:sep]
-	msg = data[sep+1:].strip()
-	if receiver in names:
-		names[receiver].send(("%s: %s" % (sender_name, msg)).encode())
-	else:
-		sender.send(("Could not find user %s" % receiver).encode())
+def get_name(sock):
+	for key, val in clients.items():
+		if val == sock:
+			return key
+	raise Exception("Name not found for sock: %s" % sock)
+
+def send(sender_name, receiver_sock, msg):
+	receiver_sock.send(("%s: %s" % (sender_name, msg)).encode())
 	
 if __name__ == "__main__":
 	router()
